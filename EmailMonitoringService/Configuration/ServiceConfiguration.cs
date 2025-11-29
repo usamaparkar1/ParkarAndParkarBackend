89388fb029ce06dd6b6f5dae1b0c64df13215737
usamaparkar1 <usamaparkar1@gmail.com>
2025-11-29 20:57:30 +0530

    Use Check interval from app settings

--- EmailMonitoringService/Configuration/ServiceConfiguration.cs
+++ EmailMonitoringService/Configuration/ServiceConfiguration.cs
@@ -4,6 +4,6 @@ public class ServiceSettings
 {
     public string Name { get; set; } = string.Empty;
     public required string Version { get; set; }
-    public int CheckInterval { get; set; }
+    public int CheckIntervalInMs { get; set; } = 3000; // Default to 3000 ms
     public bool CanRunService { get; set; }
 }
--- EmailMonitoringService/Worker.cs
+++ EmailMonitoringService/Worker.cs
@@ -1,16 +1,31 @@
+using Microsoft.Extensions.Options;
+using EmailMonitoringService.Configuration;
+
 namespace EmailMonitoringService;
 
-public class Worker(ILogger<Worker> logger) : BackgroundService
+public class Worker : BackgroundService
 {
+    private readonly ILogger<Worker> _logger;
+    private readonly ServiceSettings _serviceSettings;
+
+    public Worker(
+        ILogger<Worker> logger,
+        IOptions<ServiceSettings> serviceSettings
+    )
+    {
+        _logger = logger;
+        _serviceSettings = serviceSettings.Value;
+    }
+
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
         while (!stoppingToken.IsCancellationRequested)
         {
-            if (logger.IsEnabled(LogLevel.Information))
+            if (_logger.IsEnabled(LogLevel.Information))
             {
-                logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
             }
-            await Task.Delay(1000, stoppingToken);
+            await Task.Delay(_serviceSettings.CheckIntervalInMs, stoppingToken);
         }
     }
 }
