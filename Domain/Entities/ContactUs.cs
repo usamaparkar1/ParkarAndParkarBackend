09e07596e33d9920e181a960ca3e4ae8a2e6beba
usamaparkar1 <usamaparkar1@gmail.com>
2025-11-30 02:38:46 +0530

    Create an abstract base entity and inherit in ContactUs

--- Domain/Entities/ContactUs.cs
+++ Domain/Entities/ContactUs.cs
@@ -1,13 +1,13 @@
+using Domain.Common;
+
 namespace Domain.Entities;
 
-public class ContactRequest
+public class ContactUs: BaseEntity
 {
     public Guid Id { get; set; }
     public required string Name { get; set; }
     public required string Email { get; set; }
     public string? Phone { get; set; }
     public required string Message { get; set; }
-    public DateTime CreatedAt { get; set; }
-    public DateTime UpdatedAt { get; set; }
     public bool IsProcessedForNotification { get; set; }
 }
